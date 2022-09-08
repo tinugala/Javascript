f2308c7dd681c4554479ff15c777b63d3c0bd249
tinugala <tinugala786@gmail.com>
2022-09-08 18:57:57 +0530

    First Commit

---
+++ Solutions-Assignments/Answer.js
@@ -0,0 +1,13 @@
+/* 1. Write a program to create a Mathematical calculation table from 0 to given number n. */
+function table(x,y){
+    for(i=0;i<=y;i++){console.log(`${x} * ${i} = ${x*i}`)}
+}
+// table(5,10)
+
+/* 2. Write a program to find the square of given number from 0 to n. */
+function square(n){
+    for(i=1;i<=n;i++){console.log(`Square of ${i} is ${i*i} `)}
+}
+// square(10)
+
+/*  */
